ddfddd98d615abd7c21b120ee161d0aeb215755a
DomineersXII <channen1titan@gmail.com>
2023-09-11 17:32:47 -0400

    random number function

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-console.log("Hello World!")
-console.log("Goodbye World!")
+
+
+function randomNumber(x,y) {
+   return  Math.floor(Math.random()*(y-x+1))+x
+ 
+}
+
+
+for (let i = 0; i < 50; i++) {
+
+console.log(randomNumber(1,10))
+
+}
